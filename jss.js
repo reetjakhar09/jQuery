c463a3451e0d76f068202a535ecc5324c5dddbbb
test <test@gmail.com>
2020-10-09 14:01:04 +0530

    form input done

--- jss.js
+++ jss.js
@@ -8,29 +8,30 @@ $(document).ready(function () {
 		
 		$(vl.subhe).each(function (keyy, vle) {
 
-			$("main section:nth-child(" + (ke + 1) + ")").append('<div><h2>  ' + vle.title + ' <button onclick=myFunction(this)>X</button> </h2></div>');
+			$("main section:nth-child(" + (ke + 1) + ")").append('<div><h2>  ' + vle.title + ' <button onclick=removeSub(this)>X</button> </h2></div>');
 
 			$(vle.form).each(function (keey, vlle) {
+				console.log('ini');
 				console.log(vlle);
+				console.log('mini');
 
 				if (vlle.input == 'SELECT') {
-					console.log('reet')
 					var xa = $('<p><label> ' + vlle.label + ' </label></p>');
 					console.log(xa)
-					var ya = $('<select class="' + vlle.clas + '"> <option>select</option> </select>').appendTo(xa);
+					var ya = $('<select class="' + vlle.clas + '"> <option>select</option> ' + vlle.option.split(',').map((opt) => { return `<option>${opt}</option>`; }) + '</select>').appendTo(xa);
 					console.log(ya)
 					$("main section:nth-child(" + (ke + 1) + ") div:nth-child(" + (keyy + 2) + ")").append(xa);
-					return;
 				}
 
-				if (vlle.input == 'TEXTAREA'){
-					console.log('choudhary')
+				else if (vlle.input == 'TEXTAREA'){
 					var at = $('<p><label> ' + vlle.label + ' </label>  <textarea  name="' + vlle.name + '"   placeholder="' + vlle.placeholder + '"   class="' + vlle.clas + '" value="' + vlle.value + '" option="' + vlle.option + '"  >' + vlle.value + '</textarea>  <button onclick=myFunction(this)>X</button> </p>');
-					console.log(at)
-					$("main section:nth-child(" + (ke+1) + ") div:nth-child(" + (ke+2) + ")").append(at);
+					$("main section:nth-child(" + (ke+1) + ") div:nth-child(" + (keey+2) + ")").append(at);
+				} 
+				else {
+
+					$("main section:nth-child(" + (ke + 1) + ") div:nth-child(" + (keyy + 2) + ")").append('<p><label> ' + vlle.label + ' </label><input type="' + vlle.input + '" name="' + vlle.name + ' " placeholder="' + vlle.placeholder + '"   class="' + vlle.clas + '" value="' + vlle.value + '" option="' + vlle.option + '" disabled="'+ vlle.dis+'">  </input> <button  onclick=myFunction(this)>X</button> </p>');
 				}
 
-				$("main section:nth-child(" + (ke + 1) + ") div:nth-child(" + (keyy + 2) + ")").append('<p><label> ' + vlle.label + ' </label><input type="' + vlle.input + '" name="' + vlle.name + '" placeholder="' + vlle.placeholder + '"   class="' + vlle.clas + '" value="' + vlle.value + '" option="' + vlle.option + '"  </input> <button  onclick=myFunction(this)>X</button> </p>');
 
 			})
 
@@ -105,10 +106,30 @@ $(document).ready(function () {
 
 		var aaa = parseInt(efg) + 1;
 		var res = option.split(",");
+
+		var dis = false;
+		var read = false;
+		var req = false;
+
+		if ($('.dis').is(':checked')) {
+			dis = true;
+			$('main section:nth-child(' + subv + ') div:nth-child(' + aaa + ') p:last-child :input').prop('disabled', true);
+
+		}
+		if ($('.read').is(':checked')) {
+			read = true;
+			$('main section:nth-child(' + subv + ') div:nth-child(' + aaa + ') p:last-child :input').prop('readonly', true);
+
+		}
+		if ($('.req').is(':checked')) {
+			req = true;
+			$('main section:nth-child(' + subv + ') div:nth-child(' + aaa + ') p:last-child :input').attr('required', true);
+		}
+
 		if (abc == 'RADIO') {
 			$(res).each(function (ky) {
 				$("main section:nth-child(" + subv + ") div:nth-child(" + aaa + ")").append('<p><label> ' + res[ky] + '  </label><input type="' + abc + '" name="' + name + '" placeholder="' + placeholder + '"   class="' + clas + '" value="' + value + '"  </input> <button  onclick=myFunction(this)>X</button> </p>');
-				arr[subv - 1].subhe[efg - 1].form.push({ 'input': "radio", 'label': res[ky], 'name': name, 'placeholder': placeholder, 'clas': clas, 'value': value, 'option': option })
+				arr[subv - 1].subhe[efg - 1].form.push({ 'dis': dis, 'read': read, 'req': req, 'input': "radio", 'label': res[ky], 'name': name, 'placeholder': placeholder, 'clas': clas, 'value': value, 'option': option })
 			})
 
 			localStorage.setItem('arr', JSON.stringify(arr));
@@ -117,7 +138,7 @@ $(document).ready(function () {
 		else if (abc == 'CHECKBOX') {
 			$(res).each(function (k) {
 				$("main section:nth-child(" + subv + ") div:nth-child(" + aaa + ")").append('<p><label> ' + res[k] + '  </label><input type="' + abc + '" name="' + name + '" placeholder="' + placeholder + '"   class="' + clas + '" value="' + value + '" option="' + option + '"  </input> <button  onclick=myFunction(this)>X</button> </p>');
-				arr[subv - 1].subhe[efg - 1].form.push({ 'input': "checkbox", 'label': res[k], 'name': name, 'placeholder': placeholder, 'clas': clas, 'value': value, 'option': option })
+				arr[subv - 1].subhe[efg - 1].form.push({ 'dis': dis, 'read': read, 'req': req, 'input': "checkbox", 'label': res[k], 'name': name, 'placeholder': placeholder, 'clas': clas, 'value': value, 'option': option })
 			})
 
 			localStorage.setItem('arr', JSON.stringify(arr));
@@ -129,35 +150,24 @@ $(document).ready(function () {
 				$(y).append("<option> " + res[v] + " </option>");
 			});
 			$("main section:nth-child(" + subv + ") div:nth-child(" + aaa + ")").append(x);
-			arr[subv - 1].subhe[efg - 1].form.push({ 'input': abc, 'label': label, 'name': name, 'placeholder': placeholder, 'clas': clas, 'value': value, 'option': option })
+			arr[subv - 1].subhe[efg - 1].form.push({'dis': dis, 'read': read, 'req': req, 'input': abc, 'label': label, 'name': name, 'placeholder': placeholder, 'clas': clas, 'value': value, 'option': option })
 			localStorage.setItem('arr', JSON.stringify(arr));
 		}
 		else if (abc == 'TEXTAREA') {
 			var xt = $('<p><label> ' + label + ' </label>  <textarea  name="' + name + '"   placeholder="' + placeholder + '"   class="' + clas + '" value="' + value + '" option="' + option + '"  >' + value + '</textarea>  </p>');
 			$("main section:nth-child(" + subv + ") div:nth-child(" + aaa + ")").append(xt);
-			arr[subv - 1].subhe[efg - 1].form.push({ 'input': abc, 'label': label, 'name': name, 'placeholder': placeholder, 'clas': clas, 'value': value, 'option': option })
+			arr[subv - 1].subhe[efg - 1].form.push({'dis': dis, 'read': read, 'req': req, 'input': abc, 'label': label, 'name': name, 'placeholder': placeholder, 'clas': clas, 'value': value, 'option': option })
 			localStorage.setItem('arr', JSON.stringify(arr));
 		}
 
 		else {
 
 			$("main section:nth-child(" + subv + ") div:nth-child(" + aaa + ")").append('<p><label> ' + label + ' </label><input type="' + abc + '" name="' + name + '" placeholder="' + placeholder + '"   class="' + clas + '" value="' + value + '" option="' + option + '"  </input> <button  onclick=myFunction(this)>X</button> </p>');
-			arr[subv - 1].subhe[efg - 1].form.push({ 'input': abc, 'label': label, 'name': name, 'placeholder': placeholder, 'clas': clas, 'value': value, 'option': option })
+			arr[subv - 1].subhe[efg - 1].form.push({'dis': dis, 'read': read, 'req': req, 'input': abc, 'label': label, 'name': name, 'placeholder': placeholder, 'clas': clas, 'value': value, 'option': option })
 			localStorage.setItem('arr', JSON.stringify(arr));
 		}
 
-		if ($('.dis').is(':checked')) {
-			$('main section:nth-child(' + subv + ') div:nth-child(' + aaa + ') p:last-child :input').prop('disabled', true);
-
-		}
-		if ($('.read').is(':checked')) {
-			$('main section:nth-child(' + subv + ') div:nth-child(' + aaa + ') p:last-child :input').prop('readonly', true);
-
-		}
-		if ($('.req').is(':checked')) {
-			$('main section:nth-child(' + subv + ') div:nth-child(' + aaa + ') p:last-child :input').attr('required', true);
-
-		}
+		
 
 
 
@@ -180,3 +190,17 @@ function myFunction(t) {
 	$(t).parent().remove();
 	window.location.reload();
 };
+
+function removeSub(t) {
+	let head = $(t).parent()[0].innerText;
+	let arr = JSON.parse(localStorage.getItem('arr'));
+	let final = [];
+	arr.forEach((element,i) => {
+		let newSubhe = element.subhe.filter(el => el.title != head.substr(0, head.length - 2))
+		// console.log(newSubhe)
+		arr[i].subhe = newSubhe;
+	});
+	localStorage.setItem('arr', JSON.stringify(arr));
+	$(t).parent().remove();
+	window.location.reload();
+}
